dd1d49707c2a64f2cc2f38f980480917fcc33893
mhmdAzzouzi <mhmdazzouzi1999@gmail.com>
2021-08-21 20:42:50 +0300

    fb-pixel test

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
+import Head from "next/head";
 import Login from "./Login";
 import Loading from "../components/Loading";
 import firebase from "firebase";
@@ -24,7 +25,28 @@ function MyApp({ Component, pageProps }) {
   if (loading) return <Loading />;
 
   if (!user) return <Login />;
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1967634773391933'); 
+            fbq('track', 'PageView');
+            `,
+          }}
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
